ac8e830196fafec3769c1afc6732d392727f77a3
thanhdev <doquocthanh@github.com>
2024-07-18 00:39:58 +0700

    add - Lesson 1: Introduce some built-in functions

---
+++ lesson_1/main.js
@@ -0,0 +1,42 @@
+/*
+Introduce some built-in functions
+    1. Alert
+    2. Console
+    3. Confirm
+    4. Prompt
+    5. Set timeout
+    6. Set interval
+*/
+
+
+// 1. Alert function
+function alertFunc() {
+    var alerts = 'Do Quoc Thanh';
+    alert(alerts);
+}
+
+// 2. Console function
+function consoleFunc() {
+    var logs = 'Do Quoc Thanh';
+    console.log(logs);
+}
+
+// 3. Confirm function
+function ConfirmFunc() {
+    confirm('Do you sure!');
+}
+
+// 4. Prompt function
+function PromptFunc(){
+    prompt('Confirm your age!');
+}
+
+// 5. Set timeout function
+// setTimeout(function(){
+//     alertFunc();
+// }, 5000);
+
+// 6. Set interval function
+// setInterval(function(){
+//     alertFunc();
+// }, 5000);
