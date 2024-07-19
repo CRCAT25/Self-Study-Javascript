12cebf290707b564c9e6ae476392b44b4f406f68
thanhdev <doquocthanh@github.com>
2024-07-20 00:50:24 +0700

    add - Lesson 6: Recursion

---
+++ lesson_6/main.js
@@ -0,0 +1,41 @@
+/*
+    Đệ quy: (Recursion)
+    1. Xác định điểm dừng
+    2. Logic handle => Tạo ra điểm dừng
+*/
+
+/*
+function countDown(num) {
+    if(num > 0){
+        console.log(num);
+        return countDown(num - 1);
+    }
+    return num;
+}
+
+countDown(10);
+*/
+
+/*
+function loop(start, end, callback) {
+    if(start < end){
+        callback(start);
+        return loop(start + 1, end, callback)
+    }
+}
+
+var array = ['JS', 'PHP', 'Ruby'];
+
+loop(0, array.length, function(index){
+    console.log(array[index]);
+})
+*/
+
+function giaiThua(number){
+    if(number > 0){
+        return number * giaiThua(number - 1);
+    }
+    return 1;
+}
+
+console.log(giaiThua(6));
